b97f06c75fac62b1ca2bc448b43ae43f0165587d
MateusAlvarenga <mateus_alvarenga@protonmail.com>
2026-09-19 18:45:32 -0300

    derivar

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("helloButton").addEventListener("click", () => {
-  document.getElementById("result").textContent = "JavaScript também está funcionando!";
+  document.getElementById("result").textContent = "JavaScript também está funcionando!!";
 });
